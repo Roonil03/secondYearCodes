d12ea3c5214dfadc4c45777399bd804afc926794
Roonil03 <67649149+Roonil03@users.noreply.github.com>
2024-08-09 23:58:56 +0530

    Update nestedForPattern.java

--- OOPs/Lab3/nestedForPattern.java
+++ OOPs/Lab3/nestedForPattern.java
@@ -1,7 +1,7 @@
 public class nestedForPattern {
     public static void main(String[] args) {
         int k=1;
-        final int n = 11;
+        final int n = 10;
         for(int i=1;k<=n;i++)
         {
             for(int j = 0;j<i&&k<=n;j++,k++)
